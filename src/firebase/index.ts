017377624660fad985850411edec4849f63abde9
Shubham Upadhyay <supadhyay@mprofit.in>
2026-03-07 11:48:17 +0530

    change

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,6 +6,6 @@ admin.initializeApp({
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   })
-})
+})    
 
 export { admin }
